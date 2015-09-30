1f80e181ffc8dca49d4c55c11b4404cb09f2b5f9
Michal Bojanowski <michal2992@gmail.com>
2015-09-30 10:56:41 +0200

    Added test for read_xml_safely with an offending PBN record

--- tests/testthat/test-read_xml_safely.R
+++ tests/testthat/test-read_xml_safely.R
@@ -1,5 +1,7 @@
 context("Safe reading of XMLs")
 
+skip_on_cran()
+
 inputs <- c(
   "<p>Some paragraph with <b>text in bold</b> which should be OK</p>",
   "<p>Some paragraph with <b>also in bold</b> with a an illegal character \u0019</p>"
@@ -11,10 +13,19 @@ test_that("proper XML is parsed correctly", {
 } )
 
 
-test_that("RESTART: improper characters are removed", {
+test_that("Characters illegal in XML are removed", {
   expect_warning( res <- read_xml_safely(inputs[2]) )
   res2 <- xml2::read_xml( gsub("\u0019", "", inputs[2]) )
   expect_equal( as.character(res), as.character(res2))
+} )
+
+
+test_that("Illegal characters are removed when reading offending record in PBN", {
+  ch <- httr::GET("https://pbn.nauka.gov.pl/OAI-PMH?verb=GetRecord&identifier=242743&metadataPrefix=pbn")
+  tt <- httr::content(ch, "text")
+  expect_error( xml2::read_xml(tt) )
+  expect_warning( xml1 <- oai:::read_xml_safely(tt) )
+  expect_warning( get_records("242743", as="raw", prefix="pbn", url="https://pbn.nauka.gov.pl/OAI-PMH") )
 } )
 
 
